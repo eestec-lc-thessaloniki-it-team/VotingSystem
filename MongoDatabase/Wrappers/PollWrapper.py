112c014de661c0a54a05602284e2eb02a1558ad5
mavridaki <mavridaki52@gmail.com>
2020-04-22 21:07:34 +0300

    Implemented Wrappers

--- MongoDatabase/Wrappers/PollWrapper.py
+++ MongoDatabase/Wrappers/PollWrapper.py
@@ -1,4 +1,10 @@
+from model.Poll import *
 
 class PollWrapper:
-    def __init__(self):
-        pass
+
+    def __init__(self, object: Poll, pollId, found = False, userFound = False, operationDone = False):
+        self.object = object
+        self.pollId = pollId
+        self.found = found
+        self.userFound = userFound
+        self.operationDone = operationDone
--- MongoDatabase/Wrappers/VotesWrapper.py
+++ MongoDatabase/Wrappers/VotesWrapper.py
@@ -1,4 +1,11 @@
+from model.Vote import *
 
 class VotesWrapper:
-    def __init__(self):
-        pass
+
+    def __init__(self, lastTimestamp, votes, named = False, found = False, userFound = False, operationDone = False):
+        self.lastTimestamp = lastTimestamp
+        self.votes = votes
+        self.named = named
+        self.found = found
+        self.userFound = userFound
+        self.operationDone = operationDone
