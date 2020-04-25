2f8487f29afa5c5f583ed02c70ac330413f28888
mavroudo <mavroudo@csd.auth.gr>
2020-04-25 10:33:55 +0300

    Added test for fill usernames

--- Tests/MongoTest/UserDBTest.py
+++ Tests/MongoTest/UserDBTest.py
@@ -1,7 +1,10 @@
 import unittest
 
 from MongoDatabase.MongoDB import MongoDB
+from MongoDatabase.Wrappers.PollWrapper import PollWrapper
 from MongoDatabase.Wrappers.UserWrapper import UserWrapper
+from MongoDatabase.Wrappers.VotesWrapper import VotesWrapper
+from model.Poll import Poll
 from model.User import User
 
 
@@ -15,6 +18,7 @@ class UserDBTest(unittest.TestCase):
         userWrapper: UserWrapper = self.connection.userDB.createNewUser(self.user1.name, self.user1.mail,
                                                                         self.user1.password)
         self.assertTrue(userWrapper.operationDone)
+        self.session_id = userWrapper.object.session_id
 
     def test_create_new_user(self):
         # check added user with same mail
@@ -79,13 +83,53 @@ class UserDBTest(unittest.TestCase):
         self.assertIsNotNone(userWrapper.object.session_id)
         # Trying to log in with mail and password and check the name if changed
         userWrapper1: UserWrapper = self.connection.userDB.logInUser(self.user1.mail, self.user1.password)
-        self.assertEqual(userWrapper1.object.name,"mavroudo_best")
-        self.assertNotEqual(userWrapper1.object.session_id,userWrapper.object.session_id)
-        self.assertEqual(userWrapper.object.password,userWrapper1.object.password)
+        self.assertEqual(userWrapper1.object.name, "mavroudo_best")
+        self.assertNotEqual(userWrapper1.object.session_id, userWrapper.object.session_id)
+        self.assertEqual(userWrapper.object.password, userWrapper1.object.password)
 
     def test_fill_user_names(self):
-        pass
+        # add 2 users, first user adds a poll, both votes the first option, get all votes, fill their usernames
+        userWrapper: UserWrapper = self.connection.userDB.createNewUser(self.user3.name, self.user3.mail,
+                                                                        self.user3.password)
+        self.assertTrue(userWrapper.operationDone)
+        self.session_id2 = userWrapper.object.session_id
+
+        (user, self.user_id) = self.connection.userDB.getUserWithSessionId(self.session_id)
+        (user2, self.user_id2) = self.connection.userDB.getUserWithSessionId(self.session_id2)
+
+        # add poll
+        self.poll: Poll = Poll("how cool are we?", ["A lot", "A lot but second", "same as 2"], named=True, unique=True,
+                               creator_user_id=self.user_id)
+        pollWrapper: PollWrapper = self.connection.pollsDB.createPoll(self.poll.question, self.poll.options,
+                                                                      self.poll.named, self.poll.unique, self.user_id)
+        self.assertTrue(pollWrapper.operationDone)
+        self.assertIsNotNone(pollWrapper.object)
+        self.poll_id = pollWrapper.pollId
+        # add votes
+        voteWrapper: VotesWrapper = self.connection.votesDB.createVote(self.user_id, self.poll_id, self.poll.named, 0)
+        self.assertTrue(voteWrapper.operationDone)
+        voteWrapper: VotesWrapper = self.connection.votesDB.createVote(self.user_id2, self.poll_id, self.poll.named, 1)
+        self.assertTrue(voteWrapper.operationDone)  # last vote wrapper should contain both votes
+        self.assertEqual(len(voteWrapper.votes), 2)
+        self.assertTrue(voteWrapper.votes.get(0)[0], self.user_id)
+        self.assertTrue(voteWrapper.votes.get(1)[0], self.user_id2)
+        # test get all votes with a wrapper that has no votes
+        response: VotesWrapper = self.connection.userDB.fillUsernames(VotesWrapper(0, None))
+        self.assertFalse(response.operationDone)
+        # test with the good wrapper
+        response: VotesWrapper = self.connection.userDB.fillUsernames(voteWrapper)
+        self.assertTrue(response.operationDone)
+        self.assertTrue(response.votes.get(0)[0], self.user1.name)
+        self.assertTrue(response.votes.get(1)[0], self.user3.name)
+        userWrapper: UserWrapper = self.connection.userDB.deleteUser(self.user3.mail)
+        self.assertTrue(userWrapper.operationDone)
+
 
     def tearDown(self) -> None:
         userWrapper: UserWrapper = self.connection.userDB.deleteUser(self.user1.mail)
         self.assertTrue(userWrapper.operationDone)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
